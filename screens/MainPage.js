d7776e179586644e7013000f3022930aaa61462c
fssarfaraz <149515646+fssarfaraz@users.noreply.github.com>
2023-11-09 14:38:46 +0400

    Update MainPage.js

--- screens/MainPage.js
+++ screens/MainPage.js
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { View, StyleSheet, ImageBackground, StatusBar } from "react-native";
-import { Image } from "expo-image";
-import { Button } from "@rneui/themed";
+import {View, StyleSheet, ImageBackground} from "react-native";
+import {Image} from "expo-image";
+import {Button} from "@rneui/themed";
 import StyleDefaultDarkModeTrue from "../components/StyleDefaultDarkModeTrue";
-import { useNavigation } from "@react-navigation/native";
-import { Color } from "../GlobalStyles";
+import {useNavigation} from "@react-navigation/native";
+import {Color} from "../GlobalStyles";
 
 const MainPage = () => {
   const navigation = useNavigation();
@@ -12,69 +12,81 @@ const MainPage = () => {
   return (
     <View style={styles.mainPage}>
       <Image
-        style={[styles.mainPageChild, styles.mainPosition]}
+        style={[styles.mainPageChild]}
         contentFit="cover"
         source={require("../assets/ellipse-14.png")}
       />
+
       <Image
-        style={[styles.mainPageItem, styles.mainPosition]}
+        style={[styles.mainPageItem]}
         contentFit="cover"
         source={require("../assets/ellipse-14.png")}
       />
+
       <ImageBackground
         style={styles.logoIcon}
         resizeMode="cover"
         source={require("../assets/logo1.png")}
       />
+
       <StyleDefaultDarkModeTrue
         styleDefaultDarkModeTrueAlignSelf="unset"
         styleDefaultDarkModeTruePosition="absolute"
         styleDefaultDarkModeTrueTop={0}
         styleDefaultDarkModeTrueLeft={9}
-        styleDefaultDarkModeTrueBackgroundColor="rgba(255, 255, 255, 0)"
+        styleDefaultDarkModeTrueBackgroundColor="null"
         styleDefaultDarkModeTrueWidth={375}
         styleDefaultDarkModeTrueMarginLeft="unset"
         styleDefaultDarkModeTrueMarginTop="unset"
       />
+
       <Button
         title="LOGIN"
         radius={18}
         iconPosition="left"
         type="solid"
-        titleStyle={styles.lOGINBUTTONBtn}
+        titleStyle={styles.logBtnText}
         onPress={() => navigation.navigate("LoginPage")}
-        containerStyle={styles.lOGINBUTTONBtn1}
-        buttonStyle={styles.lOGINBUTTONBtn2}
+        containerStyle={styles.logBtnCont}
+        buttonStyle={styles.logBtn}
       />
+
       <Button
         title="SIGN UP"
         radius={18}
         iconPosition="left"
         type="solid"
-        titleStyle={styles.sIGNUPBUTTONBtn}
+        titleStyle={styles.sigBtnText}
         onPress={() => navigation.navigate("RegistrationPage")}
-        containerStyle={styles.sIGNUPBUTTONBtn1}
-        buttonStyle={styles.sIGNUPBUTTONBtn2}
+        containerStyle={styles.sigBtnCont}
+        buttonStyle={styles.sigBtn}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  lOGINBUTTONBtn: {
+const styles = StyleSheet.create(
+  {
+  logBtnText: 
+  {
     color: "#fff",
     fontSize: 20,
     fontWeight: "300",
     fontFamily: "Nunito-Light",
   },
-  lOGINBUTTONBtn1: {
-    left: 23,
+
+  logBtnCont: 
+  {
+    left: 26,
     top: 701,
     position: "absolute",
   },
-  lOGINBUTTONBtn2: {
+
+  logBtn: 
+  {
     shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: {
+    shadowOffset: 
+    {
       width: 0,
       height: 4,
     },
@@ -84,48 +96,73 @@ const styles = StyleSheet.create({
     width: 151,
     height: 61,
   },
-  sIGNUPBUTTONBtn: {
+
+  sigBtnText: 
+  {
     color: "#fff",
     fontSize: 20,
     fontWeight: "300",
     fontFamily: "Nunito-Light",
   },
-  sIGNUPBUTTONBtn1: {
-    left: 219,
+
+  sigBtnCont: 
+  {
+    left: 230,
     top: 701,
     position: "absolute",
   },
-  sIGNUPBUTTONBtn2: {
+
+  sigBtn: 
+  {
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOffset: 
+    {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 4,
+    elevation: 4,
+    shadowOpacity: 1,
     width: 151,
     height: 61,
   },
-  mainPosition: {
-    width: 400,
-    left: 0,
+    
+  mainPageChild: 
+  {
+    top: -150,
+    height: 600,
+    width: 500,
+    position: "absolute",
+    left: 130,
+  },
+
+  mainPageItem: 
+  {
+    top: 450,
+    height: 600,
+    width: 500,
+    left: -146,
     position: "absolute",
   },
-  mainPageChild: {
-    top: 0,
-    height: 455,
-  },
-  mainPageItem: {
-    top: 422,
-    height: 430,
-  },
-  logoIcon: {
+
+  logoIcon: 
+  {
     top: 174,
     left: 38,
     width: 318,
     height: 318,
     position: "absolute",
   },
-  mainPage: {
+
+  mainPage: 
+  {
     backgroundColor: Color.labelDarkPrimary,
     flex: 1,
     width: "100%",
     height: 852,
     overflow: "hidden",
   },
-});
+}
+);
 
 export default MainPage;
