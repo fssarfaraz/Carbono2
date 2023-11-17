3774c1d2a06d1aea0e7dd9a771c6f336ef776df0
Reinhard Joy <33929978@student.mmurdoch.edu.au>
2023-11-17 11:14:45 +0400

    Bakar Commented - Game screen

--- screens/Games.js
+++ screens/Games.js
@@ -14,7 +14,8 @@ const Games = () => {
   const handleNavigation = (screen) => {
     navigation.navigate(screen);
   };
-
+  
+  //  This is a demo comment to test the git commit
   const gamesData = [
     { id: 1, name: "Space Voyagers 1", image: require("../assets/egs-destiny2lightfall-bungie-addon-g1a-00-1920x1080484927180fa94217cdce9e1bd5aa5e9f-1.png") },
     { id: 2, name: "Space Voyagers 2", image: require("../assets/23brc4s1egslauncherpdp2560x14402560x144070cf344c9005-2.png") },
