1a1b3fdb4a15134120a577f83d1ac95cef450aa3
Reinhard Joy <33929978@student.mmurdoch.edu.au>
2023-11-30 16:08:38 +0400

    Search + Assets + Library bottom nav change

--- screens/SearchQuery.js
+++ screens/SearchQuery.js
@@ -1,285 +1,199 @@
-import * as React from "react";
-import { Image } from "expo-image";
-import { StyleSheet, Pressable, View, StatusBar, Text } from "react-native";
-import { Button } from "@rneui/themed";
-import Property1HomeImage from "../components/Property1HomeImage";
+import React, { useState } from "react";
+import { Image, Pressable, Text, TextInput, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import FormContainer1 from "../components/FormContainer1";
 import StyleDefaultDarkModeTrue from "../components/StyleDefaultDarkModeTrue";
-import Search1 from "../components/Search1";
-import { Color, FontFamily, FontSize, Padding, Border } from "../GlobalStyles";
+import { Color, Padding } from "../GlobalStyles";
 
 const SearchQuery = () => {
   const navigation = useNavigation();
+  const [search, setSearch] = useState("");
+
+  const handleNavigation = (screen) => {
+    navigation.navigate(screen);
+  };
 
   return (
-    <View style={styles.searchQuery}>
-      <Image
-        style={[styles.searchQueryChild, styles.searchPosition]}
-        contentFit="cover"
-        source={require("../assets/ellipse-3.png")}
-      />
-      <Image
-        style={[styles.searchQueryItem, styles.searchPosition]}
-        contentFit="cover"
-        source={require("../assets/ellipse-3.png")}
-      />
-      <Property1HomeImage
-        imageDimensions={require("../assets/navigation-barr1.png")}
-        property1HomeIconPosition="absolute"
-        property1HomeIconWidth={402}
-        property1HomeIconHeight={106}
-        property1HomeIconTop={746}
-        property1HomeIconLeft={0}
-      />
-      <View style={[styles.iconPersonOutlineParent, styles.nameFlexBox]}>
-        <Pressable
-          style={styles.iconLayout2}
-          onPress={() =>
-            navigation.navigate("BottomTabsRoot", { screen: "UserProfile" })
-          }
-        >
-          <Image
-            style={styles.icon}
-            contentFit="cover"
-            source={require("../assets/-icon-person-outline.png")}
-          />
+    <View style={styles.container}>
+      {/* Background Images */}
+      <Image style={styles.backgroundImage} source={require("../assets/ellipse-3.png")} />
+      <Image style={styles.backgroundImage2} source={require("../assets/ellipse-3.png")} />
+
+      {/* Back Button */}
+      <View style={styles.backButtonContainer}>
+        <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
+          <FontAwesome5 name="chevron-left" size={30} color="#01427A" />
         </Pressable>
-        <Image
-          style={[styles.iconBookSaved, styles.iconLayout1]}
-          contentFit="cover"
-          source={require("../assets/-icon-book-saved.png")}
-        />
-        <Pressable
-          style={[styles.iconDiscussion, styles.iconLayout2]}
-          onPress={() =>
-            navigation.navigate("BottomTabsRoot", { screen: "Forum" })
-          }
+      </View>
+
+      {/* Title */}
+      <Text style={styles.title}>REDUCING YOUR FOOTPRINT</Text>
+
+      {/* Search Input */}
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.search}
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search"
+          placeholderTextColor="#fff"
+          fontSize={18}
+          zIndex={32}
+        /> 
+        <Pressable 
+          onPress={() => handleNavigation("SearchResults")}
+          style={styles.searchIcon}
         >
-          <Image
-            style={styles.icon}
-            contentFit="cover"
-            source={require("../assets/-icon-discussion.png")}
-          />
+          <Image style={styles.searchIcon} resizeMode="cover" source={require("../assets/search.png")} />
         </Pressable>
-        <Image
-          style={[styles.iconGameControllerOutline, styles.iconLayout1]}
-          contentFit="cover"
-          source={require("../assets/-icon-game-controller-outline1.png")}
-        />
+        {/* <Image style={styles.searchIcon} resizeMode="cover" source={require("../assets/search.png")} /> */}
       </View>
-      <View style={styles.iconCalculatorWrapper}>
-        <Image
-          style={styles.iconCalculator}
-          contentFit="cover"
-          source={require("../assets/-icon-calculator.png")}
-        />
+
+      {/* Bottom Navigation */}
+      <View style={styles.bottomNavBar}>
+        {/* Navigation Icons */}
+        <Pressable onPress={() => handleNavigation("UserProfile")}>
+          <Image style={styles.bottomNavIcon} source={require("../assets/-icon-person-outline.png")} />
+        </Pressable>
+        <Pressable onPress={() => handleNavigation("Educational")}>
+          <Image style={styles.bottomNavIcon} source={require("../assets/-icon-book-saved3.png")} />
+        </Pressable>
+        <Pressable onPress={() => handleNavigation("Forum")}>
+          <Image style={styles.bottomNavIcon} source={require("../assets/-icon-discussion.png")} />
+        </Pressable>
+        <Pressable onPress={() => handleNavigation("Games")}>
+          <Image style={styles.bottomNavIcon} source={require("../assets/-icon-game-controller-outline6.png")} />
+        </Pressable>
       </View>
-      <FormContainer1
-        dimensionCode={require("../assets/rectangle.png")}
-        carDimensions={require("../assets/rectangle1.png")}
-        productCode={require("../assets/rectangle2.png")}
-      />
-      <StyleDefaultDarkModeTrue
-        styleDefaultDarkModeTrueAlignSelf="unset"
-        styleDefaultDarkModeTruePosition="absolute"
-        styleDefaultDarkModeTrueTop={10}
-        styleDefaultDarkModeTrueLeft={9}
-        styleDefaultDarkModeTrueBackgroundColor="rgba(255, 255, 255, 0)"
-        styleDefaultDarkModeTrueWidth={375}
-        styleDefaultDarkModeTrueMarginLeft="unset"
-        styleDefaultDarkModeTrueMarginTop="unset"
-      />
-      <View style={[styles.name, styles.nameFlexBox]}>
-        <Text style={styles.reducingYourFootprint}>
-          Reducing Your Footprint
-        </Text>
-      </View>
-      <View style={styles.searchQueryInner} />
-      <Search1 />
-      <Pressable
-        style={[styles.loremIpsumDolorContainer, styles.loremPosition]}
-        onPress={() => navigation.navigate("SearchResults")}
-      >
-        <Text style={styles.loremTypo}>{`Lorem ipsum dolor sit amet.....
 
-Lorem ipsum dolor sit amet.....
+      {/* Surface Icon */}
+      <Image style={styles.surfaceIcon} resizeMode="cover" source={require("../assets/navigation-barr2.png")} />
 
-Lorem ipsum dolor sit amet.....
-
-Lorem ipsum dolor sit amet.....`}</Text>
+      {/* Calculator Icon */}
+      <Pressable onPress={() => handleNavigation("Calculator")} style={styles.iconCalculatorParent}>
+        <Image style={styles.iconCalculator} resizeMode="cover" source={require("../assets/-icon-calculator.png")} />
       </Pressable>
-      <Text style={[styles.loremIpsum, styles.loremTypo]}>Lorem Ipsum</Text>
-      <Image
-        style={[styles.arrow1Icon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../assets/arrow-1.png")}
-      />
-      <Image
-        style={[styles.arrow4Icon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../assets/arrow-1.png")}
-      />
-      <Image
-        style={[styles.arrow3Icon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../assets/arrow-1.png")}
-      />
-      <Image
-        style={[styles.arrow2Icon, styles.iconLayout]}
-        contentFit="cover"
-        source={require("../assets/arrow-1.png")}
-      />
-      <Button
-        radius="5"
-        iconPosition="left"
-        type="clear"
-        color="#fff"
-        icon={{ name: "chevron-left", type: "material-community" }}
-        onPress={() => navigation.goBack()}
-        containerStyle={styles.materialSymbolsarrowBackIoIconBtn}
-        buttonStyle={styles.materialSymbolsarrowBackIoIconBtn1}
-      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  materialSymbolsarrowBackIoIconBtn: {
-    left: 9,
-    top: 54,
-    position: "absolute",
-  },
-  materialSymbolsarrowBackIoIconBtn1: {
-    width: 30,
-    height: 30,
-    overflow: "hidden",
-  },
-  searchPosition: {
-    width: 400,
-    left: 0,
-    position: "absolute",
-  },
-  nameFlexBox: {
-    flexDirection: "row",
-    position: "absolute",
-  },
-  iconLayout1: {
-    width: 33,
-    marginLeft: 72,
-  },
-  iconLayout2: {
-    height: 30,
-    width: 30,
-  },
-  loremPosition: {
-    left: 65,
-    position: "absolute",
-  },
-  loremTypo: {
-    color: Color.labelDarkPrimary,
-    fontFamily: FontFamily.nunitoRegular,
-    lineHeight: 20,
-    fontSize: FontSize.body15Regular_size,
-    textAlign: "left",
-  },
-  iconLayout: {
-    height: 16,
-    width: 16,
-    left: 336,
-    position: "absolute",
-  },
-  searchQueryChild: {
-    top: 0,
-    height: 394,
-  },
-  searchQueryItem: {
-    top: 435,
-    height: 417,
-  },
-  icon: {
-    height: "100%",
-    width: "100%",
-  },
-  iconBookSaved: {
-    height: 31,
-    marginLeft: 72,
-  },
-  iconDiscussion: {
-    marginLeft: 72,
-  },
-  iconGameControllerOutline: {
-    height: 24,
-    marginLeft: 72,
-  },
-  iconPersonOutlineParent: {
-    top: 807,
-    left: 24,
-    width: 385,
-    alignItems: "flex-end",
-  },
-  iconCalculator: {
-    width: 41,
-    height: 45,
-  },
-  iconCalculatorWrapper: {
-    top: 746,
-    left: 170,
-    padding: Padding.p_3xs,
-    position: "absolute",
-  },
-  reducingYourFootprint: {
-    fontSize: FontSize.size_8xl,
-    fontWeight: "600",
-    fontFamily: FontFamily.nunitoSemiBold,
-    width: 310,
-    textAlign: "left",
-  },
-  name: {
-    top: 90,
-    left: 31,
-    width: 372,
-    height: 42,
-    alignItems: "center",
-  },
-  searchQueryInner: {
-    height: "24.41%",
-    width: "86.77%",
-    top: "16.67%",
-    right: "5.6%",
-    bottom: "58.92%",
-    left: "7.63%",
-    borderRadius: Border.br_xl,
-    backgroundColor: Color.colorDarkslateblue_300,
-    position: "absolute",
-  },
-  loremIpsumDolorContainer: {
-    top: 190,
-  },
-  loremIpsum: {
-    top: 157,
-    left: 65,
-    position: "absolute",
-  },
-  arrow1Icon: {
-    top: 182,
-  },
-  arrow4Icon: {
-    top: 301,
-  },
-  arrow3Icon: {
-    top: 261,
-  },
-  arrow2Icon: {
-    top: 221,
-  },
-  searchQuery: {
-    backgroundColor: Color.labelDarkPrimary,
+const styles = {
+  container: {
     flex: 1,
+    backgroundColor: Color.labelDarkPrimary,
     height: 852,
     overflow: "hidden",
     width: "100%",
   },
-});
+  backgroundImage: {
+    height: 330,
+    width: 400,
+    right: 100,
+    position: "absolute",
+  },
+  backgroundImage2: {
+    height: 630,
+    width: 600,
+    left: 100,
+    position: "absolute",
+    top: 430,
+  },
+  backButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute",
+    top: 54,
+    left: 16,
+    zIndex: 30,
+  },
+  backButton: {
+    flex: 1,
+    width: "100%",
+    overflow: "hidden",
+    padding: 10,
+  },
+  title: {
+    top: 120,
+    fontSize: 22,
+    fontWeight: "600",
+    fontFamily: "Nunito-SemiBold",
+    textAlign: "left",
+    position: "absolute",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  searchContainer: {
+    top: 160,
+    left: 30,
+    position: "absolute",
+    width: "80%",
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "rgba(1, 66, 122, 0.7)",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+    zIndex: 32,
+  },
+  search: {
+    borderRadius: 20,
+    flex: 1,
+    width: "100%",
+    height: 40,
+    fontFamily: "Nunito-Regular",
+    fontSize: 16,
+    color: "#fff", // Add this line for text color
+  },
+  searchIcon: {
+    flex: 1,
+    width: 20,
+    height: 20,
+    position: "absolute",
+    right: 10,
+    zIndex: 32,
+  },
+  bottomNavBar: {
+    flex: 1,
+    flexDirection: "row",
+    height: 70,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingBottom: 10,
+    position: "relative",
+    zIndex: 3,
+  },
+  bottomNavIcon: {
+    width: 30,
+    height: 30,
+    top: 365,
+    zIndex: 30,
+  },
+  iconCalculator: {
+    top: 728,
+    width: 40,
+    height: 45,
+    alignSelf: "center",
+    position: "absolute",
+    zIndex: 2,
+  },
+  iconCalculatorParent: {
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+    padding: 10,
+    zIndex: 3,
+  },
+  surfaceIcon: {
+    flex: 1,
+    width: "100%",
+    height: 135,
+    position: "absolute",
+    bottom: 0,
+    zIndex: 1,
+  },
+};
 
 export default SearchQuery;
