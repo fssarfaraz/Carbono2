90a0fae6d0c2577fd7829c35d82867a882ef5ec4
fssarfaraz <34366459@student.murdoch.edu.au>
2023-11-30 21:30:48 +0400

    forum 2

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,58 +1,97 @@
 import * as React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, FlatList, Pressable } from "react-native";
 import { Image } from "expo-image";
 import CommentContainer from "./CommentContainer";
 import { FontSize, FontFamily, Color, Border, Padding } from "../GlobalStyles";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+import {getDatabase, ref, set, onValue, update} from "firebase/database";
+import { getAuth} from 'firebase/auth';
+import { app } from "../App";
+import { Button } from "@rneui/base";
+import { ScrollView } from "react-native-gesture-handler";
 
 const CardContainer = ({post}) => {
 
+  const database = getDatabase();
+  const auth = getAuth(app);
+
   const [allComments, setComments] = useState([]);
+  
+  const getComments = () => {
+    const commentRef = ref(database, `Comments/`);
 
-  /*
-  const getComments = useCallback((post) => {
-    const postsRef = ref(database, 'posts/');
-
-    onValue(postsRef, (snapshot) => {
-      // Find matching user  
-      const posts = snapshot.val();
-      const email = post.email;
-      const matchingPost = Object.values(posts).find((u) => u.email.toLowerCase() === email);
-
-      if (matchingPost) 
+    onValue(commentRef, (snapshot) => {
+      const data = snapshot.val();
+      //filtering data to only those of the current user and then storing id, date and result in array
+      const commentsss = Object.entries(data).filter(([key, value]) => value.postID === post.id)
+      .map(([id, entry]) => ({
+        id: entry.id,
+        x: entry.comment,  
+        y: entry.likes,
+        z: entry.date,
+        a: entry.name,
+      }));
+      //if data is not empty
+      if (commentsss.length > 0) 
       {
-        const emailParts = email.split('@');
-        // Get first part (before "@")
-        const emailName = emailParts[0];
-        const key = `${matchingPost.date}-${emailName}-${matchingPost.title}`;
-        update(ref(database, 'posts/' + key), 
-        {
-          likes: post.likes + 1,
-          comments: post.comments,
-          date: post.date,
-          email: post.email,
-          post: post.post,
-          title: post.title
-        });
-        console.log('Like added');
+        console.log('Comments:', commentsss);
+        setComments(commentsss);
+        console.log('All Comments:', allComments);
       } 
       else 
       {
-        console.log('Post not found in the database');
+        console.log("No comments found for post");
+        setComments([]);
       }
     });
-    setLikes(post.likes + 1);
-  }, [post]);
-  */
+  };
+
+  const updateCommentLikes = async (item) => {
+    const key = item.id;
+    let newLikes = item.likes + 1;
+    await update(ref(database, 'Comments/' + key), 
+    {
+      likes: newLikes,
+    });
+    console.log('Like added');
+  };
 
   return (
     <View style={[styles.comments, styles.recentFlexBox]}>
+      <View style = {{top: 25, left: 210}}>
+        <Button style={styles.loadCommButton} 
+        title="Load Comments" 
+        type="clear"
+        titleStyle={styles.regsBtnText}
+        onPress={() => { getComments()}}/>
+      </View>
       <View style={[styles.sectionTitle, styles.comment3FlexBox]}>
-        <Text style={styles.comments45}>Comments {post.comments}</Text>
-        <View style={[styles.recent, styles.recentFlexBox]}>
+      <Text style={styles.comments45}>Comments {allComments.length > 0 ? `(${allComments.length})` : '(0)'}</Text>
+        <View style={{top: 20, left: -105}}>
+        {allComments.length > 0 && (
+          <FlatList
+          data={allComments}
+          renderItem={({item}) => (<View style={{top: 0, left: 0, height: 140, width: 400, marginBottom: -40}}>
+            <Text style={{fontSize: 15, fontFamily: FontFamily.nunitoLight}}> {item.a} </Text>
+            <Text style={{fontFamily: FontFamily.nunitoRegular, fontSize: 15, top: 3}}> {item.x} </Text>
+            <Text style={{fontFamily: FontFamily.nunitoLight, fontSize: 12, top: 10, left: 2, color: "#727477"}}> {item.z} </Text>
+            <Text style={{fontFamily: FontFamily.nunitoLight, fontSize: 12, left: 100, top: -6, color: "#727477"}}> {item.y} likes </Text>
+            <View style={{top: 0}}>
+              <Pressable onPress={() => updateCommentLikes(item)}>
+                  <Image
+                    style={styles.vectorIcon}
+                    contentFit="cover"
+                    source={require("../assets/like-1.png")}
+                  />
+              </Pressable>
+            </View>
+          </View>
+           )}
+          keyExtractor={(item) => item.id}
+          />
+          )}
         </View>
       </View>
-      
     </View>
   );
 };
@@ -156,6 +195,23 @@ const styles = StyleSheet.create({
     top: 301,
     left: 33,
     justifyContent: "center",
+  },
+  vectorIcon: {
+    height: 14,
+    width: 14,
+    top: -70,
+    left: 340,
+  },
+  loadCommButton: {
+    width: 100,
+    position: "absolute",
+    borderRadius: Border.br_11xl,
+  },
+  regsBtnText: 
+  {
+    color: "#01427A",
+    fontSize: 14,
+    fontFamily: FontFamily.nunitoRegular
   },
 });
 
