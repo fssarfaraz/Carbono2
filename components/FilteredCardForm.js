d46f729e624b60c15b07f3b7e73075870b87a6d6
fssarfaraz <149515646+fssarfaraz@users.noreply.github.com>
2023-12-01 17:01:59 +0400

    Comment FilteredCardForm.js

--- components/FilteredCardForm.js
+++ components/FilteredCardForm.js
@@ -1,3 +1,9 @@
+/*
+Date: 19/11/2023
+Component: FilteredCardForm.js
+Purpose: This component holds and displays post details such as post content and post likes. Used in ForumView.js
+*/
+
 import * as React from "react";
 import { Image } from "expo-image";
 import { StyleSheet, Text, View, Pressable } from "react-native";
@@ -22,13 +28,14 @@ const FilteredCardForm = ({post, role}) => {
   const database = getDatabase();
   const auth = getAuth(app);
 
+  //get the current user's name from the database
   const getName = () => {
     const userRef = ref(database, 'users/');
     const user = auth.currentUser; 
     const email = user.email; 
 
     onValue(userRef, (snapshot) => {
-      // Find matching user  
+      // Find matching user by comparing emails
       const users = snapshot.val();
       const matchingUser = Object.values(users).find((u) => u.email.toLowerCase() === email);
 
@@ -43,17 +50,22 @@ const FilteredCardForm = ({post, role}) => {
     })
   }
 
+  //call getName when page loads
   useEffect(() => {
     getName();
   }, [database]);
 
+  //function to update post likes in the database
   const updatePostLikes = async () => {
     const email = post.email;
     const emailParts = email.split('@');
     // Get first part (before "@")
     const emailName = emailParts[0];
+    //key of the post that is being liked
     const key = `${post.date}-${emailName}-${post.title}`;
+    //increment likes
     let newLikes = post.likes + 1;
+    //update database
     await update(ref(database, 'posts/' + key), 
     {
       likes: newLikes,
@@ -61,21 +73,25 @@ const FilteredCardForm = ({post, role}) => {
     console.log('Like added');
     setLikes(post.likes + 1);
   };
-  
+
+  //toggle menu for moderators
   const toggleMenu = () => {
     setShouldShowMenu(!shouldShowMenu);
     console.log(shouldShowMenu);
   };
 
+  //function to delete reported posts for moderators
   const deletePost = async () =>  {
     const postsRef = ref(database, "posts/");
     let postId;
     console.log('delete');
+    //snapshot of the posts in the database
     onValue(postsRef, (snapshot) => {
       const data = snapshot.val();
       console.log("Post data:", data);
       // console.log("Current user:", auth.currentUser.email);
       console.log("Post title:", post.title);
+      //find current post
       postId = Object.keys(data).find(
             (key) => data[key].title.toLowerCase() === post.title.toLowerCase()
           );
@@ -83,6 +99,7 @@ const FilteredCardForm = ({post, role}) => {
     });
 
     // await database().ref(`posts/${postId}`).remove();
+    //set current post to null
     await set(ref(database, `posts/${postId}`), null);
     navigation.goBack();
   }
@@ -98,14 +115,17 @@ const FilteredCardForm = ({post, role}) => {
                 contentFit="cover"
                 source={require("../assets/profile-photo.png")}
               />
+              {/*Display poster name*/}
               <View style={styles.jacobWashingtonParent}>
                 <Text style={[styles.jacobWashington, styles.ifYouThinkTypo]}>
                   {name}
                 </Text>
+                  {/*Display post date*/}
                 <Text style={[styles.mAgo, styles.mAgoTypo]}>{post.date}</Text>
               </View>
             </View>
             <View>
+              {/*Display menu to delete post*/}
               <Pressable onPress={toggleMenu}>
                 <Image
                   style={styles.iconDotsVertical}
@@ -115,6 +135,7 @@ const FilteredCardForm = ({post, role}) => {
               </Pressable>
             </View>
 
+            {/*If moderator, allow to delete post*/}
             {shouldShowMenu ? (
               <View>
                 <Pressable onPress={deletePost}>
@@ -123,10 +144,13 @@ const FilteredCardForm = ({post, role}) => {
               </View>
             ) : null}
           </View>
+
+          {/*Display post content*/}
           <Text
             style={[styles.ifYouThink, styles.ifYouThinkTypo]}
           >{post.post}</Text>
 
+          {/*Press thumbs up image to like post. Press will call function updatePostLikes*/}
           <View style={[styles.postActions, styles.profileInfoFlexBox]}>
             <View style={styles.infoFlexBox}>
               <Pressable onPress={() => updatePostLikes()}>
@@ -145,6 +169,7 @@ const FilteredCardForm = ({post, role}) => {
                 contentFit="cover"
                 source={require("../assets/icon--comment.png")}
               />
+              {/*Display no of comments*/}
               <Text style={[styles.text, styles.mAgoTypo]}>{post.comments}</Text>
             </View>
           </View>
